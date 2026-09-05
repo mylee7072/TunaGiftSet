dcf0104c1faf6e2a0902600cb70ecac9db1e6752
mylee7072 <mylee707200@gmail.com>
2026-09-05 21:08:45 +0900

    Configure PostgreSQL database connection

---
+++ src/main/java/studio/aroundhub/tunagiftset/TunaGiftSetApplication.java
@@ -0,0 +1,13 @@
+package studio.aroundhub.tunagiftset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunaGiftSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunaGiftSetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/studio/aroundhub/tunagiftset/TunaGiftSetApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.aroundhub.tunagiftset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunaGiftSetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
